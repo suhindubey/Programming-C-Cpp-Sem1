0e78641ed6a2d2ade1fd488a0fdffa9b009a1369
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-02 16:36:48 +0530

    Create Digit to month.c

---
+++ Digit to month.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter any number 0 to 9 :");
+	scanf("%d", &a);
+	switch(a)
+	{
+		case 0:
+			printf ("\n zero");
+			break;
+		
+		case 1:
+			printf("\n one");
+			break;
+			
+		case 2:
+			printf("\n two");
+			break;
+			
+		case 3:
+			printf("\n three");
+			break;
+			
+		case 4:
+			printf("\n four");
+			break;
+			
+			
+		case 5:
+			printf("\n five");
+			break;
+			
+		case 6:
+			printf("\n six");
+			break;
+			
+		case 7:
+			printf("\n seven");
+			break;	
+			
+		case 8:
+			printf("\n eight");
+			break;
+			
+		case 9:
+			printf("\n nine");
+			break;		
+									
+	}
+}
