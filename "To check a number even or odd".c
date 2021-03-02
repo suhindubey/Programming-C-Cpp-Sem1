62d1c888a0a1492aaba4ce85f1c9973f0fc249fd
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-02 15:32:18 +0530

    Create "To check a number even or odd".c

---
+++ "To check a number even or odd".c
@@ -0,0 +1,14 @@
+//to check whether a given number is even or odd. 
+#include <stdio.h>
+
+void main()
+{
+	int m;
+	printf("Enter the number\n");
+	scanf ("%d", &m);
+	
+	if ((m%2)==0)
+		printf("M is even\n ");
+	else
+		printf("M is odd\n ");
+}
